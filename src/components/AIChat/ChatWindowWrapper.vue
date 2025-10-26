feb436f36240ab75f7fcb8110db6dad560aee981
johnxer <johnxer@gmail.com>
2025-10-26 15:12:19 +0100

    Styling chat window

--- src/components/AIChat/ChatWindowWrapper.vue
+++ src/components/AIChat/ChatWindowWrapper.vue
@@ -1,15 +1,62 @@
 <template>
-    <div class="fixed bottom-22 right-6 w-84 h-120 bg-white shadow-xl rounded-2xl">
+    <div class="fixed bottom-22 right-6 w-84 h-120 bg-white shadow-xl rounded-xl border border-gray-100">
         <div class="flex flex-col h-full">
-            <ul class="p-4 overflow-auto">
+            <ul
+                v-if="chatMessages"
+                class="p-4 overflow-auto space-y-4 grow-1"
+            >
 
                 <li
                     v-for="chatMessage in chatMessages"
                     :key="chatMessage.date"
                 >
-                    {{ chatMessage.user }}
-                    {{ chatMessage.question }}
-                    {{ chatMessage.time }}
+                    <!-- <div class="flex justify-between gap-2 text-xs">
+                        <span>
+                            {{ chatMessage.user }}
+                        </span>
+                        <span>
+                            {{ chatMessage.date }}
+                        </span>
+                    </div> -->
+                    <div
+                        class="relative p-3 rounded-bl-2xl rounded-br-2xl text-sm"
+                        :class="chatMessage.user === 'AI adviser' ?
+                            'bg-primary-100 rounded-tr-2xl after:content-[\'\'] after:absolute after:top-0 after:border-t-[0] after:border-b-[12px] after:border-r-[0.5rem] after:border-transparent after:border-r-primary-100 after:-left-2' :
+                            'bg-primary-500 rounded-tl-2xl after:content-[\'\'] after:absolute after:top-0 after:border-t-[0] after:border-b-[12px] after:border-l-[0.5rem] after:border-transparent after:border-l-primary-500 after:-right-2'"
+                    >
+                        <div
+                            class="text-xs mb-1 flex items-center gap-1"
+                            :class="chatMessage.user === 'AI adviser' ?
+                                'text-primary-700/50' :
+                                'text-white/75'"
+                        >
+                            <span class="material-symbols-outlined text-base">
+                                
+                                {{ 
+                                    chatMessage.user === 'AI adviser' ? 'network_intelligence' : 'face'
+                                    }}
+                                
+                            </span>
+                            {{ chatMessage.user }}
+                        </div>
+                        <p
+                            class="text-sm"
+                            :class="chatMessage.user === 'AI adviser' ?
+                                'text-primary-700' :
+                                'text-white'"
+                        >
+                            {{ chatMessage.message }}
+                        </p>
+                        <div 
+                            class="text-[10px] text-end italic"
+                            :class="chatMessage.user === 'AI adviser' ?
+                                'text-primary-700/50' :
+                                'text-white/75'"
+                        >
+
+                            {{ chatMessage.date }}
+                        </div>
+                    </div>
                 </li>
                 <!-- <li>
                     <div>
@@ -67,7 +114,7 @@
                 <input
                     type="text"
                     placeholder="Write your question..."
-                    class="p-4 w-full focus:outline-0"
+                    class="px-4 py-3 w-full focus:outline-0 bg-gray-50 focus:bg-gray-50 transition-colors duration-600 rounded-b-xl text-sm"
                     @keyup.enter="submitQuestion"
                     :disabled="isLoading"
                     v-model.trim="question"
@@ -101,6 +148,19 @@ const question = ref('')
 
 const chatMessages = ref([])
 
+chatMessages.value = [
+    {
+        user: 'johnxer@gmail.com',
+        message: 'Lorem ipsum',
+        date: '22.11.1981'
+    },
+    {
+        user: 'AI adviser',
+        message: 'Lorem ipsum',
+        date: '11.12.2858'
+    }
+]
+
 
 const email = computed(() => user.value?.email);
 
@@ -109,7 +169,7 @@ const submitQuestion = async () => {
 
     const userData = {
         user: email.value,
-        question: question.value,
+        message: question.value,
         date: new Date()
     }
 
@@ -122,7 +182,7 @@ const submitQuestion = async () => {
 
         const aiData = {
             user: 'AI adviser',
-            question: answer.value,
+            message: answer.value,
             date: new Date()
         }
 
