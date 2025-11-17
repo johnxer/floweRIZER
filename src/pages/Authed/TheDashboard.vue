aaa0eab3302311fb92f0b0b7ef4e51aaaae25480
johnxer <johnxer@gmail.com>
2025-11-17 14:08:24 +0100

    Added empty rooms/plants dashboard placeholder

--- src/pages/Authed/TheDashboard.vue
+++ src/pages/Authed/TheDashboard.vue
@@ -1,5 +1,6 @@
 <template>
     <base-container>
+
         <transition
             name="fade"
             mode="out-in"
@@ -9,6 +10,26 @@
                 class="flex items-center justify-center absolute top-[60px] md:bottom-[64px] h-[calc(100vh-60px-64px)] inset-x-[0] w-screen"
                 loader-size="lg"
             />
+            <div
+                v-else-if="showPlaceholder"
+                class="max-w-[300px] mx-auto text-center"
+            >
+                <h2 class="text-xl mb-4 text-gray-700">
+                    You have no rooms yet...
+                </h2>
+                <base-button
+                    @click="roomsStore.openAddModal"
+                    btn-style="notRoundedMd"
+                    btn-size="base"
+                    :btn-full-width="false"
+                    class="inline-flex gap-1"
+                >
+                    <span class="material-symbols-outlined text-2xl">
+                        add
+                    </span>
+                    Add a new room
+                </base-button>
+            </div>
             <div v-else>
                 <stats-box class="mb-10" />
                 <div class="grid md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-10 lg:gap-8 items-start">
@@ -33,6 +54,7 @@
                     </button>
                 </transition>
             </div>
+
 
 
             <!-- TO BE STYLED and make work -->
@@ -64,15 +86,16 @@
 
 import { computed, nextTick, watch } from 'vue';
 
+import BaseButton from '../../components/Base/BaseButtons/BaseButton.vue';
 import BaseContainer from '../../components/Base/BaseContainer.vue';
 import BaseLoader from '../../components/Base/BaseLoader.vue';
 import BaseRoom from '../../components/Base/BaseRoom.vue';
 
-
 import StatsBox from '../../components/Stats/StatsBox.vue';
 import TheModals from '../../components/TheModals.vue';
 
-import { useGetData } from '../../composables/useGetData';
+import { ref } from 'vue';
+import { useGetAllPlants, useGetData } from '../../composables/useGetData';
 import { useRoomsStore } from '../../stores/useRoomsStore';
 import { useScrollStore } from '../../stores/useScrollStore';
 
@@ -84,10 +107,16 @@ const {
 
 const roomsStore = useRoomsStore()
 
+const hasRooms = ref(false);
+
 watch(() => rooms.value, newVal => {
     roomsStore.rooms = newVal;
-
-    console.log(newVal)
+    if (newVal.length > 1) {
+        hasRooms.value = true;
+    } else {
+        hasRooms.value = false;
+    }
+    console.log('rooms', newVal.length)
 })
 
 const sortedRooms = computed(() => {
@@ -129,6 +158,28 @@ watch(
     },
     { deep: true }
 )
+
+const {
+    items: plants,
+    error: errorPlants,
+    isPending: isPendingPlants
+} = useGetAllPlants()
+
+
+const hasPlants = ref(false)
+
+watch(plants, newVal => {
+    if (!newVal.length) {
+        console.log('No plants')
+        hasPlants.value = false
+    } else {
+        console.log('plants', newVal)
+        hasPlants.value = true
+    }
+})
+
+const showPlaceholder = computed(() => !hasRooms.value && !hasPlants.value)
+
 
 </script>
 
